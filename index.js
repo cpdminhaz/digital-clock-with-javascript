dc360d716ad499bb7be887758617d67219e6439c
Md Minhazul Abedin <98245064+minhazfav@users.noreply.github.com>
2023-11-30 22:46:29 +0600

    this is a bullshit

---
+++ index.js
@@ -0,0 +1,21 @@
+// function secondCounter(){
+//     let second = 0;
+//         setInterval(function(){
+//             second++
+//         }, 1000);
+//         let seconds = second;
+//         document.getElementById("second").innerText = seconds;
+//     }
+// secondCounter();
+
+// function minuteCounter(){
+//     let minute = 0;
+//         setInterval(function(){
+//             if(minute > 59){
+//                 document.getElementById("minute").innerText = 0;
+//             }
+//             minute++
+//             document.getElementById("minute").innerText = minute;
+//         }, 15000);
+//     }
+// minuteCounter();
